0589e1875a1d0a0073c5a0228b750d0fe8734b40
Evan <ehutch7@gmail.com>
2023-11-22 02:37:42 +0000

    Testing out prod env

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
 import { fullConfig } from "./configs";
 
+/**
+ * TODO: expose function that accepts path to tsconfig to apply to the parser
+ * AND get rid of more dependencies
+ * MAKE sure CI/CD is using NODE_ENV=production
+ */
 const configs = {
   full: fullConfig,
 };
